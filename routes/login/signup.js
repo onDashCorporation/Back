0a0d1a9adab60c2202ff15c989a49d207b28929e
annaagabi <annaa0478@gmail.com>
2024-05-08 16:11:23 -0300

    Feat: atualização do cadastro

--- routes/login/signup.js
+++ routes/login/signup.js
@@ -179,6 +179,39 @@ router.post('/', upload.single('foto_usu'), (req, res) => {
     })
 })
 
+router.get('/', (req, res) => {
+    const sql = "SELECT * FROM usuarios";
+    db.query(sql, (err, data) => {
+        if (err) {
+            return res.status(500).json({
+                error: err.message
+            });
+        } else {
+            res.status(200).json(data);
+        }
+    });
+});
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    const sql = "SELECT usuId, usuNome, email, fk_cargoId, fk_depId FROM usuarios WHERE usuId = ?";
+    const values = [id];
+
+    db.query(sql, values, (err, data) => {
+        if (err) {
+            return res.status(500).json({
+                error: err.message
+            });
+        }
+        if (data.length === 0) {
+            return res.status(404).json({
+                message: 'Usuário não encontrado'
+            });
+        }
+        res.status(200).json(data[0]);
+    });
+});
+
 router.put('/:id', upload.single('foto_usu'), (req, res) => {
     const id = req.params.id;
     const {
@@ -261,4 +294,26 @@ router.put('/:id', upload.single('foto_usu'), (req, res) => {
         })
     })
 })
+
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+    const sql = "DELETE FROM usuarios WHERE usuId = ?";
+    const values = [id];
+
+    db.query(sql, values, (err, data) => {
+        if (err) {
+            return res.status(500).json({
+                error: err.message
+            });
+        }
+        if (data.length === 0) {
+            return res.status(404).json({
+                message: 'Usuário não encontrado'
+            });
+        }
+        res.status(200).json({
+            message: 'Dados deletados do sistema com sucesso'
+        });
+    });
+});
 module.exports = router;
