4255307d4c9ee8f0a1b0f87aac1c222dfa073517
carlos-venancio <hencarlos42@gmail.com>
2024-05-17 11:46:11 -0300

    feat: adicionando salvamento de imagens na propria api

--- routes/produto/cadItem.js
+++ routes/produto/cadItem.js
@@ -25,6 +25,9 @@ const multer = require('multer')
 const path = require('path')
 
 const storage = multer.diskStorage({
+    destination: (req,file,cb) => {
+        cb(null,"../../img")
+    },
     filename: (req, file, cb) => {
         cb(null, file.fieldname + "_" + Date.now() + path.extname(file.originalname))
     }
