e3625450b894a08e8707f6209c3f9ae91144f1c7
carlos-venancio <hencarlos42@gmail.com>
2024-05-17 11:19:45 -0300

    feat: adicionando inserts e possibilidade de conexão remota com o banco de dados

--- db.js
+++ db.js
@@ -1,23 +1,20 @@
 const mysql = require('mysql2');
 
 require('dotenv')
-.config()
+    .config()
 
 function createDBConnection() {
     const db = mysql.createConnection({
-        host: "localhost",
-        //process.env.HOST_DATABASE || 
-        user: 'root',
-        //process.env.MYSQL_USER || 
-        password: '',
-        //process.env.MYSQL_PASSWORD || 
-        database: 'onDash'
-        //process.env.MYSQL_DATABASE || 
+        host: process.env.HOST_DATABASE || "localhost",
+        user: process.env.MYSQL_USER || 'root',
+        password: process.env.MYSQL_PASSWORD || '',
+        database: 'onDash',
+        port: 3306,
     });
-    
+
     return db;
 }
 
 
- 
+
 module.exports = createDBConnection;
