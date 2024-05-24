9f017be19f7ff63432c2976db03f97732a56ccef
carlos-venancio <--global>
2024-05-24 11:57:03 -0300

    feat: conexão com banco de dados por meio de pool

--- db.js
+++ db.js
@@ -15,5 +15,24 @@ function createDBConnection() {
 }
 
 
+// tempo de persistencia de inatividade de uma conexão
+const connection_lifetime = 60 * 60 * 10 // 10 minutos
+
+async function getDBConnection() {
+    const db = mysql.createPool({
+        host: process.env.HOST_DATABASE || "localhost",
+        user: process.env.MYSQL_USER || 'root',
+        password: process.env.MYSQL_PASSWORD || '',
+        database: 'onDash',
+        waitForConnections: true,
+        connectionLimit: 2,
+        enableKeepAlive: true,
+        keepAliveInitialDelay: connection_lifetime,
+        queueLimit: 0
+    });
+
+    return await db.promise().getConnection();
+}
+
 
 module.exports = createDBConnection;
